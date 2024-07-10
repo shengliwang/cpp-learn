b7df2c565f03dd247bb6e6dd8aac42aca6e655a4
wangshengli <vicking@mail.ustc.edu.cn>
2024-07-10 23:34:28 +0800

    学习转义，字符串，布尔，数据输入

---
+++ code/15cin.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+int main(void){
+	int i;
+	//std::cout << "i = " << i << std::endl;
+	//std::cin >> i;
+	//std::cout << "new i = " << i << std::endl;
+	float f = 3.14f;
+	//std::cout << "f = " << f << std::endl;
+	//std::cin >> f;
+	//std::cout << "new f = " << f << std::endl;
+	char c = 'a';
+	//std::cout << "c = " << c << std::endl;
+	//std::cin >> c;
+	//std::cout << "new c = " << c << std::endl;
+
+	std::string str = "hello";
+//	std::cout << "str = " << str << std::endl;
+//	std::cin >> str;
+//	std::cout << "new str = " << str << std::endl;
+
+	bool flag = false;
+	std::cout << "flag = " << flag << std::endl;
+	std::cin >> flag;
+	std::cout << "new flag = " << flag << std::endl;
+
+	return 0;
+}
